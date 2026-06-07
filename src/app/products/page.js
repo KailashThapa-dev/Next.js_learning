5e59d7b301f2d67bb3cffb97354a6a11245c65c8
Kailash <kailassthapa73@gmail.com>
2026-06-07 15:31:57 +0545

    Complete the params, searchParams and metdata

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,6 +1,10 @@
 import React from 'react'
+ export const metadata ={
+  title: "Product",
 
-const ProductPage = async () => {
+}
+const ProductPage = async ({searchParams}) => {
+  console.log(await searchParams);
   const res = await fetch("https://jsonplaceholder.typicode.com/todos");
   const products = await res.json();
   console.log(products);
